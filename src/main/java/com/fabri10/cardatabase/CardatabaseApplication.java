89261a1790c6045a53cf13fea0c255d1112750ab
Fabrizio <fabricio.garcia@outlook.com>
2023-05-31 13:22:44 -0600

    feat: Modify car entity to include owners and modify sample objects

--- src/main/java/com/fabri10/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/fabri10/cardatabase/CardatabaseApplication.java
@@ -2,6 +2,8 @@ package com.fabri10.cardatabase;
 
 import com.fabri10.cardatabase.domain.Car;
 import com.fabri10.cardatabase.domain.CarRepository;
+import com.fabri10.cardatabase.domain.Owner;
+import com.fabri10.cardatabase.domain.OwnerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,12 +11,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class CardatabaseApplication implements CommandLineRunner {
     private static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
 
     @Autowired
-    private CarRepository repository;
+    private CarRepository carRepository;
+
+    @Autowired
+    private OwnerRepository ownerRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CardatabaseApplication.class, args);
@@ -23,12 +30,18 @@ public class CardatabaseApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2021, 59000));
-        repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2019, 29000));
-        repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2020, 39000));
+        // Add owner objects and save these to db
+        Owner owner1 = new Owner("John", "Johnson");
+        Owner owner2 = new Owner("Mary", "Robinson");
+        ownerRepository.saveAll(Arrays.asList(owner1, owner2));
+
+        // Add car object and link to owners and save these to db
+        carRepository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2021, 59000, owner1));
+        carRepository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2019, 29000, owner2));
+        carRepository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2020, 39000, owner2));
 
 
-        for (Car car : repository.findAll()) {
+        for (Car car : carRepository.findAll()) {
             logger.info(car.getBrand() + " " + car.getModel());
         }
     }
--- src/main/java/com/fabri10/cardatabase/domain/Car.java
+++ src/main/java/com/fabri10/cardatabase/domain/Car.java
@@ -17,7 +17,7 @@ public class Car {
     public Car() {
     }
 
-    public Car(String brand, String model, String color, String registerNumber, int year, int price) {
+    public Car(String brand, String model, String color, String registerNumber, int year, int price, Owner owner) {
         super();
         this.brand = brand;
         this.model = model;
@@ -25,6 +25,7 @@ public class Car {
         this.registerNumber = registerNumber;
         this.year = year;
         this.price = price;
+        this.owner = owner;
 
     }
 
